ab06ad3eb6d961526f52af9a0aa27e9175718006
willianbarata <willianbarata@gmail.com>
2022-05-07 20:39:04 -0300

    Etapa 2 - Ordenando Via bubble sort

---
+++ Colecoes/Helper/OperacoesArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecoes.Helper
+{
+    internal class OperacoesArray
+    {
+        public void OrdenarBubbleSort(ref int[] array)
+        {
+            int temp = 0;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j = 0; j < array.Length - 1; j++)
+                {
+                    if(array[j] > array[j + 1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];    
+                        array[j] = temp;
+                        
+                    }
+                }   
+            }
+        }
+        public void ImprimirArray(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -1,39 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-int[] arrayInteiros = new int[3];
+using Colecoes.Helper;
 
-arrayInteiros[0] = 1;
-arrayInteiros[1] = 2;
-arrayInteiros[2] = 3;
+OperacoesArray op = new OperacoesArray();
 
-Console.WriteLine("Percorrendo Array pelo For");
-for(int i = 0; i < arrayInteiros.Length; i++)
-{
-    Console.WriteLine(arrayInteiros[i]);
-}
+int[] array = new int[5] {6, 3 ,8 ,1, 9};
 
-Console.WriteLine("Percorrendo Array pelo Foreach");
-foreach (int i in arrayInteiros)
-{
-    Console.WriteLine(i);
-}
+Console.WriteLine("Array Sem ordenação");
+op.ImprimirArray(array);
 
-Console.WriteLine("Impressão da Matriz");
+Console.WriteLine("Array ordenado");
+op.OrdenarBubbleSort(ref array);
+op.ImprimirArray(array);
 
-int[,] matriz = new int[4, 2]
-{
-    { 1 ,100 },    
-    { 2 ,200 },  
-    { 3 ,300 },  
-    { 4 ,400 }
-};
+//int[] arrayInteiros = new int[3];
 
-for(int i = 0;i < matriz.GetLength(0); i++)
-{
-    for(int j = 0;j < matriz.GetLength(1); j++)
-    {
-        Console.WriteLine(matriz[i, j]);
-    }
-    
-}
+//arrayInteiros[0] = 1;
+//arrayInteiros[1] = 2;
+//arrayInteiros[2] = 3;
+
+//Console.WriteLine("Percorrendo Array pelo For");
+//for(int i = 0; i < arrayInteiros.Length; i++)
+//{
+//    Console.WriteLine(arrayInteiros[i]);
+//}
+
+//Console.WriteLine("Percorrendo Array pelo Foreach");
+//foreach (int i in arrayInteiros)
+//{
+//    Console.WriteLine(i);
+//}
+
+//Console.WriteLine("Impressão da Matriz");
+
+//int[,] matriz = new int[4, 2]
+//{
+//    { 1 ,100 },    
+//    { 2 ,200 },  
+//    { 3 ,300 },  
+//    { 4 ,400 }
+//};
+
+//for(int i = 0;i < matriz.GetLength(0); i++)
+//{
+//    for(int j = 0;j < matriz.GetLength(1); j++)
+//    {
+//        Console.WriteLine(matriz[i, j]);
+//    }
+
+//}
